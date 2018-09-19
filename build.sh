32e0525e143cfd400f7d30c93bdc488714c91d1c
Carlos Feria <carlosthe19916@gmail.com>
2018-09-19 17:32:30 -0500

    add changes

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+ng build --prod --aot --build-optimizer --vendor-chunk=true
+docker build -t openfact/openfact-search-pe .
+docker build -t openfact/openfact-search-pe-openshift -f Dockerfile.deploy .
+docker tag openfact/openfact-search-pe openfact/openfact-search-pe:$(git rev-parse --short HEAD);
+docker tag openfact/openfact-search-pe-openshift openfact/openfact-search-pe-openshift:$(git rev-parse --short HEAD);
+docker push openfact/openfact-search-pe:$(git rev-parse --short HEAD)
+docker push openfact/openfact-search-pe-openshift:$(git rev-parse --short HEAD)
+docker push openfact/openfact-search-pe:latest
+docker push openfact/openfact-search-pe-openshift:latest
--- src/config/of.config.ts
+++ src/config/of.config.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public api = 'http://localhost:8080/api/public/documents';
+    public api = 'http://openfactv2-openfact-inpath.apps.console.sistcoop.org:83/api/public/documents';
 }
