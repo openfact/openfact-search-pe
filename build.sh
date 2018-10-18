0c1df95c78b2779fb093be3bc35399cfda2531bb
Carlos Feria <carlosthe19916@gmail.com>
2018-10-18 12:10:52 -0500

    change build command

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ng build --prod --aot --build-optimizer --vendor-chunk=true
+ng build --prod --aot --build-optimizer --vendor-chunk=true --output-path=dist
 docker build -t openfact/openfact-search-pe .
 docker build -t openfact/openfact-search-pe-openshift -f Dockerfile.deploy .
 docker tag openfact/openfact-search-pe openfact/openfact-search-pe:$(git rev-parse --short HEAD);
